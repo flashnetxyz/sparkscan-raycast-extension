bf2fa0e7b57eba55a334bc0e8be1e53f4ef0f07b
Nejc Drobnic <nejc@flashnet.xyz>
2025-07-27 16:14:39 +0200

    feat: adds sparkscan matching logic
    Adds new matchers for different address types on Sparkscan.
    
    *   Introduces a base `Match` class for shared functionality.
    *   Implements specific matchers for:
        *   Spark addresses
        *   Transactions
        *   Tokens
        *   Public Keys
    *   Updates the main function to use these matchers to identify the input type.

---
+++ src/lib/matchers/_base.ts
@@ -0,0 +1,21 @@
+export abstract class Match {
+  public readonly search: string;
+  public network: "MAINNET" | "REGTEST";
+  
+  protected $matched = false;
+
+  public constructor(search: string, network: "MAINNET" | "REGTEST") {
+    this.search = search;
+    this.network = network;
+  }
+
+  public abstract match(): boolean;
+  public get matched(): boolean {
+    return this.$matched;
+  }
+  public abstract get matchedNetwork(): "MAINNET" | "REGTEST" | null;
+
+  public abstract get path(): string;
+}
+
+export type Matchers = Match[];
---
+++ src/lib/matchers/index.ts
@@ -0,0 +1,6 @@
+export type { Matchers } from './_base';
+
+export { TransactionMatch } from './tx';
+export { PublicKeyMatch } from './publicKey';
+export { TokenMatch } from './token';
+export { SparkAddressMatch } from './sparkAddress';
